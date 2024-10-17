334ce745f95f7a397ba1fb9b5c2d7c71b4b62d38
Binxu Wang <binxu_wang@hms.harvard.edu>
2024-10-16 23:04:44 -0400

    add ImageNet mean cov

--- img_dataset_mean_cov_all.py
+++ img_dataset_mean_cov_all.py
@@ -1,7 +1,3 @@
-/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/Datasets/EDM_datasets/imgdataset_PCAs
-
-
-
 """
 
 """
@@ -139,6 +135,46 @@ plt.title("Eigenvalues of AFHQv2-64 dataset")
 plt.savefig(join(PCAdir, "afhqv264_PCA_eigval.png"))
 plt.show()
 
+
+#%% ImageNet-64
+batch_size = 1024
+INet_dir = join(dataset_dir, "imagenet-64x64.zip")
+dataset = ImageFolderDataset(INet_dir, resolution=64)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=0)
+# example batch
+imgshape = dataset[0][0].shape
+imgmean = torch.zeros(np.prod(imgshape)).cuda()
+imgcov = torch.zeros(np.prod(imgshape), np.prod(imgshape)).cuda()
+imgbatch, _ = next(iter(dataloader))
+for imgbatch, _ in tqdm.tqdm(dataloader): # 18sec
+    imgbatch = imgbatch.cuda().float() / 255.0
+    imgbatch = Resize(64)(imgbatch)
+    imgbatch = imgbatch.view(imgbatch.shape[0], -1)
+    imgmean += imgbatch.sum(dim=0)
+    imgcov += imgbatch.T @ imgbatch
+imgmean /= len(dataset)
+imgcov -= imgmean.outer(imgmean) * len(dataset)
+imgcov /= len(dataset) - 1
+# eigen decomposition
+eigval, eigvec = torch.linalg.eigh(imgcov, )
+#%%
+torch.save({"eigval": eigval, "eigvec": eigvec, "imgmean": imgmean, },
+           join(PCAdir, "imagenet64_PCA.pt"))
+#%%
+mtg = ToPILImage()(make_grid(torch.flipud(eigvec[:, -64:].t()).view(-1, *imgshape)))
+mtg.save(join(PCAdir, "imagenet64_PCA_top64.png"))
+#%%
+plt.imshow(mtg)
+plt.axis('off')
+plt.tight_layout()
+plt.show()
+#%%
+plt.semilogy(eigval.cpu().numpy()[::-1])
+plt.xlabel("eigenvalue index")
+plt.ylabel("eigenvalue")
+plt.title("Eigenvalues of imagenet-64 dataset")
+plt.savefig(join(PCAdir, "imagenet64_PCA_eigval.png"))
+plt.show()
 
 
 
